a1c525fe9a30fdd4f6ca26ad24f6f5fd9e39e703
Martin <martingeo15@gmail.com>
2019-07-31 10:37:29 +0530

    added method cancel. Changed variable name remove to booked. implemented var myseat

--- train_booking.py
+++ train_booking.py
@@ -28,6 +28,8 @@ class Passenger:
         pno_base += 1
         self.pno = pno_base
 
+        self.myseat = None
+
     def book(self, seatno):
         if available_seats is False:
             print('No seats available.\nPushing into wait queue')
@@ -36,15 +38,27 @@ class Passenger:
         elif seatno in available_seats:
             print('seat no.{} booked by passenger {}'.format(
                 seatno, self.name))
-            remove = available_seats.pop(
+            booked = available_seats.pop(
                 available_seats.index(seatno))
-            booked_seats.append(remove)
+
+            self.myseat = booked
+            
+            booked_seats.append(booked)
             
         else:
             print('seat already booked')
             print_seats(available_seats)
             seatno = int(input('New seat for booking : '))
             self.book(seatno)
+
+    def cancel(self):
+        cindex = booked_seats.index(self.myseat)
+        canceled = booked_seats.pop(cindex)
+
+        print('Your ticket has been canceled')
+
+        available_seats.insert(canceled - 1, canceled)
+        
             
 if __name__ == '__main__':
     p1 = Passenger('martin')
@@ -52,3 +66,7 @@ if __name__ == '__main__':
 
     p1.book(3)
     p2.book(3)
+
+    p1.cancel()
+
+    print(available_seats)
